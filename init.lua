cd8e5f4248318b1b1ae2f8fc54a58c387c214c0a
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2021-06-14 19:16:08 +0200

    Automatically align on backspace

--- init.lua
+++ init.lua
@@ -1,11 +1,18 @@
 local backspace = function(tabwidth)
-  tabwidth = tabwidth or vis.win.tabwidth or 1
-  if tabwidth == nil then
+  tabwidth = tabwidth or vis.win.tabwidth
+  if tabwidth == nil or win.selection.pos == 0 then
     vis:feedkeys('<vis-delete-char-prev>')
   else
-    vis:command('?\n|' .. string.rep(' ', tabwidth) .. '|.?d')
+    local sel = win.selection
+    local l, r = win.file:match_at(lpeg.P(" ") ^ 1, sel.pos - 1, 200)
+    if l ~= nil and r ~= nil then
+      local pos = sel.pos
+      local width = ((r - l - 1) % tabwidth) + 1
+      vis:feedkeys(string.rep('<vis-delete-char-prev>', width))
+    else
+      vis:feedkeys('<vis-delete-char-prev>')
+    end
   end
-  vis:feedkeys('<vis-mode-insert>')
 end
 
 vis.events.subscribe(vis.events.INIT, function()

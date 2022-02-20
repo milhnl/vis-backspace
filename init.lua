23c20d83821aed567f3865f634c4c060d3c4eeea
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2022-02-20 19:18:01 +0100

    Fix backspace behaviour

--- init.lua
+++ init.lua
@@ -3,15 +3,16 @@ local backspace = function(tabwidth)
   if tabwidth == nil or win.selection.pos == 0 then
     vis:feedkeys('<vis-delete-char-prev>')
   else
-    local sel = win.selection
-    local l, r = win.file:match_at(lpeg.P(" ") ^ 1, sel.pos - 1, 200)
-    if l ~= nil and r ~= nil then
+    for sel in win:selections_iterator() do
       local pos = sel.pos
-      local width = ((r - l - 1) % tabwidth) + 1
-      vis:feedkeys(string.rep('<vis-delete-char-prev>', width))
-    else
-      vis:feedkeys('<vis-delete-char-prev>')
+      local l, r = win.file:match_at(lpeg.P(" ") ^ 1, pos - 1, 200)
+      if l ~= nil and r ~= nil then
+        win.file:delete(l, (r - l - 1) % tabwidth + 1)
+      else
+        win.file:delete(pos - 1, 1)
+      end
     end
+    vis.win:draw()
   end
 end
 
